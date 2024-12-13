9159ee3c79f0d8e059772cb1cba4a18b7bca5f96
Matt Micene (RHT) <mmicene@redhat.com>
2024-12-13 11:16:33 -0500

    Discard non-matching lines, startup info from gdbus monitor

--- auto_power_profile.sh
+++ auto_power_profile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#	act_profile=$(gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Get 'net.hadess.PowerProfiles' 'ActiveProfile' | awk -F "[<'>]" '{ print $3 }')
+#	batt_state=$(gdbus call --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower --method org.freedesktop.DBus.Properties.Get 'org.freedesktop.UPower' 'OnBattery' | awk -F "[<>]" '{ print $2 }')
+#	echo $act_profile $batt_state
 
 set_profile () {
 	echo "Setting active profile to $1"
@@ -7,15 +10,14 @@ set_profile () {
 }
 
 gdbus monitor --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower | while read LINE; do
-#	act_profile=$(gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Get 'net.hadess.PowerProfiles' 'ActiveProfile' | awk -F "[<'>]" '{ print $3 }')
-#	batt_state=$(gdbus call --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower --method org.freedesktop.DBus.Properties.Get 'org.freedesktop.UPower' 'OnBattery' | awk -F "[<>]" '{ print $2 }')
-#	echo $act_profile $batt_state
 	batt_state=$(echo $LINE | awk -F "[<>]" '{ print $2 }')
+	if [[ ! -z "$batt_state" ]]; then 
 	if [ "$batt_state" = "true" ]; then
 		echo "On battery power" && set_profile balanced
 	elif [ "$batt_state" = "false" ]; then
 		echo "On AC power" && set_profile performance
 	else
 		echo "Unknown battery state found!"
+	fi
 	fi
 done
