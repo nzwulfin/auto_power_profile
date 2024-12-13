e7684d557565a90fd70000f84fc2e8d7de385ea8
Matt Micene (RHT) <mmicene@redhat.com>
2024-12-13 11:12:13 -0500

    Just monitor battery state, set to balanced on battery, performance on ac, and let gnome handle low power

--- auto_power_profile.sh
+++ auto_power_profile.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-dbus-monitor --system "type='signal',path='/org/freedesktop/UPower/devices/battery_BAT0',member='PropertiesChanged'" | while read LINE; do
-    echo ${LINE} | grep battery_BAT0 | grep -q PropertiesChanged
-    if [ $? -eq 0 ]; then
-        BATT_STAT=$(dbus-send --print-reply=literal --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/devices/battery_BAT0 org.freedesktop.DBus.Properties.Get  string:org.freedesktop.UPower.Device string:State | awk '{ print $3; }')
-        if [ $BATT_STAT -eq 1 ] || [ $BATT_STAT -eq 4 ]; then
-            LEVEL=$(powerprofilesctl list | grep -q performance && echo "performance" || echo "balanced")
-        elif [ $BATT_STAT -eq 5 ]; then
-            LEVEL="balanced"
-        else
-            LEVEL="power-saver"
-        fi
-	echo "Changing power level to ${LEVEL}"
-	gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Set 'net.hadess.PowerProfiles' 'ActiveProfile' "<'${LEVEL}'>" > /dev/null
-	[[ $? -ne 0 ]] && echo "Could not change power level to ${LEVEL}!"
-    fi
+set_profile () {
+	echo "Setting active profile to $1"
+	gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Set 'net.hadess.PowerProfiles' 'ActiveProfile' "<'$1'>" > /dev/null
+	[[ $? -ne 0 ]] && echo "Could not change power level!"
+}
+
+gdbus monitor --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower | while read LINE; do
+#	act_profile=$(gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Get 'net.hadess.PowerProfiles' 'ActiveProfile' | awk -F "[<'>]" '{ print $3 }')
+#	batt_state=$(gdbus call --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower --method org.freedesktop.DBus.Properties.Get 'org.freedesktop.UPower' 'OnBattery' | awk -F "[<>]" '{ print $2 }')
+#	echo $act_profile $batt_state
+	batt_state=$(echo $LINE | awk -F "[<>]" '{ print $2 }')
+	if [ "$batt_state" = "true" ]; then
+		echo "On battery power" && set_profile balanced
+	elif [ "$batt_state" = "false" ]; then
+		echo "On AC power" && set_profile performance
+	else
+		echo "Unknown battery state found!"
+	fi
 done
