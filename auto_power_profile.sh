ce3ced4925ad6c2d4ae278539157f80461d5e1f1
Matt Micene (RHT) <mmicene@redhat.com>
2024-12-13 16:53:30 -0500

    Move the check to a function, run once before monitor. Closes #3

--- auto_power_profile.sh
+++ auto_power_profile.sh
@@ -8,16 +8,22 @@ set_profile () {
 	gdbus call --system --dest net.hadess.PowerProfiles --object-path /net/hadess/PowerProfiles --method org.freedesktop.DBus.Properties.Set 'net.hadess.PowerProfiles' 'ActiveProfile' "<'$1'>" > /dev/null
 	[[ $? -ne 0 ]] && echo "Could not change power level!"
 }
-
-gdbus monitor --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower | while read LINE; do
-	batt_state=$(echo $LINE | awk -F "[<>]" '{ print $2 }')
-	if [[ ! -z "$batt_state" ]]; then 
-	if [ "$batt_state" = "true" ]; then
+check_state () {
+	if [[ ! -z "$1" ]]; then 
+	if [ "$1" = "true" ]; then
 		echo "On battery power" && set_profile balanced
-	elif [ "$batt_state" = "false" ]; then
+	elif [ "$1" = "false" ]; then
 		echo "On AC power" && set_profile performance
 	else
 		echo "Unknown battery state found!"
 	fi
 	fi
+}
+
+start_state=$(gdbus call --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower --method org.freedesktop.DBus.Properties.Get 'org.freedesktop.UPower' 'OnBattery' | awk -F "[<>]" '{ print $2 }')
+check_state $start_state
+
+gdbus monitor --system --dest org.freedesktop.UPower --object-path /org/freedesktop/UPower | while read LINE; do
+	batt_state=$(echo $LINE | awk -F "[<>]" '{ print $2 }')
+	check_state $batt_state
 done
